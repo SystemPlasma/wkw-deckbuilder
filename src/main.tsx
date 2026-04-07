d5ea2c656349a4c6858c416d08fec54608d7250e
SystemPlasma <systemplasma13@gmail.com>
2026-04-07 14:55:55 -0500

    Restore Tailwind styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css';
 import App from './App';
 
 const container = document.getElementById('root');
@@ -9,4 +10,3 @@ if (!container) {
 
 const root = createRoot(container);
 root.render(<App />);
-
