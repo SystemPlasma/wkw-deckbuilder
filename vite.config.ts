aa1d19683de31eb855d9dc84ca2eecc757fcd948
SystemPlasma <systemplasma13@gmail.com>
2026-04-14 22:51:51 -0500

    Updates

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// GitHub Pages project site served at /wkw-deckbuilder/
-export default defineConfig({
-  plugins: [react()],
-  base: '/wkw-deckbuilder/',
-});
-
